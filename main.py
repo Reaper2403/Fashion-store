310ee035e24f5598cd04afa958e40d1bd37cd6b4
Punyam Singh <punyamsingh04@gmail.com>
2023-04-15 12:13:24 +0530

    fashion store commit 1

---
+++ main.py
@@ -0,0 +1,99 @@
+from tkinter import *
+from PIL import ImageTk, Image
+from tkinter import messagebox
+import mysql.connector
+
+# Connect to the database
+mydb = mysql.connector.connect(
+    host="localhost",
+    user="sqluser",
+    password="password",
+    database="ananya"
+)
+
+root = Tk()
+root.title('LogIn')
+root.geometry('925x480')
+root.config(bg='#ffffff')
+root.resizable(False, False)
+root.iconbitmap('send_message_icon_251493.ico')
+
+def signin():
+    # Get the username and password entered by the user
+    username = user.get()
+    password = word.get()
+
+    # Execute the SQL query to check if the user exists in the database
+    mycursor = mydb.cursor()
+    mycursor.execute("SELECT * FROM users WHERE username = %s AND password = %s", (username, password))
+    result = mycursor.fetchone()
+
+    if result:
+        # If the user exists, run the store functionality from store.py
+        root.destroy()
+        from store import run_store
+        run_store()
+
+    else:
+        # If the user does not exist, show an error message
+        messagebox.showerror('Invalid', 'Invalid Username or Password')
+
+        # If the user wants to register, open the registration window
+        if messagebox.askyesno('Register', 'Do you want to register?'):
+            root.destroy()
+            import register
+
+def on_enter(e):
+    user.delete(0, 'end')
+
+def on_leave(e):
+    name = user.get()
+    if name == '':
+        user.insert(0, 'Username')
+
+img = Image.open('images.png')
+img = img.resize((460, 450), Image.LANCZOS)
+my_img = ImageTk.PhotoImage(img)
+my_label = Label(root, image=my_img).place(x=40, y=50)
+
+frame = Frame(root, width=350, height=350, bg='white')
+frame.place(x=480, y=70)
+
+heading = Label(frame, text='Sign In', fg='blue', bg='white', font='Arimo,25,bold')
+heading.place(x=150, y=5)
+
+user = Entry(frame, width=25, fg='black', borderwidth=0, bg='white', font='Arimo,11')
+user.place(x=30, y=80)
+user.insert(0, 'Username')
+user.bind('<FocusIn>', on_enter)
+user.bind('<FocusOut>', on_leave)
+
+Frame(frame, width=295, height=2, bg='black').place(x=25, y=107)
+
+def on_enter(e):
+    word.delete(0, 'end')
+
+def on_leave(e):
+    name = word.get()
+    if name == '':
+        word.insert(0, 'Password')
+
+word = Entry(frame, width=25, fg='black', borderwidth=0, bg='white', font='Arimo,11')
+word.place(x=30, y=150)
+word.insert(0, 'Password')
+word.bind('<FocusIn>', on_enter)
+word.bind('<FocusOut>', on_leave)
+
+Frame(frame, width=295, height=2, bg='black').place(x=25, y=177)
+
+Button(frame, width=39, pady=7, text='Sign In',
+command=lambda: signin(), fg='white', bg='#0047ab', font='Arimo,12,bold').place(x=30, y=220)
+
+def open_register_window(event):
+    root.destroy()
+    import register
+
+register_label = Label(frame, text='Register new employee here', fg='blue', bg='white', font='Arimo,10,underline')
+register_label.place(x=100, y=280)
+register_label.bind('<Button-1>', open_register_window)
+root.mainloop()
---
+++ register.py
@@ -0,0 +1,133 @@
+from tkinter import *
+from tkinter import messagebox
+import mysql.connector
+
+# Connect to the database
+mydb = mysql.connector.connect(
+    host="localhost",
+    user="sqluser",
+    password="password",
+    database="ananya"
+)
+
+root = Tk()
+root.title('Register')
+root.geometry('400x400')
+root.config(bg='#ffffff')
+root.resizable(False, False)
+root.iconbitmap('send_message_icon_251493.ico')
+
+
+def register():
+    # Get the user details entered by the user
+    username = user.get()
+    password = word.get()
+    fname = first_name.get()
+    lname = last_name.get()
+    dob = date_of_birth.get()
+    
+    # Execute the SQL query to insert the new user into the database
+    mycursor = mydb.cursor()
+    sql = "INSERT INTO users (username, password, F_name, l_name, dob) VALUES (%s, %s, %s, %s, %s)"
+    val = (username, password, fname, lname, dob)
+    mycursor.execute(sql, val)
+    mydb.commit()
+
+    # Show a success message and clear the fields
+    messagebox.showinfo('Success', 'Registration Successful!')
+    user.delete(0, END)
+    word.delete(0, END)
+    first_name.delete(0, END)
+    last_name.delete(0, END)
+    date_of_birth.delete(0, END)
+
+def on_enter(e):
+    user.delete(0, 'end')
+
+def on_leave(e):
+    name = user.get()
+    if name == '':
+        user.insert(0, 'Username')
+
+def on_enter1(e):
+    word.delete(0, 'end')
+
+def on_leave1(e):
+    name = word.get()
+    if name == '':
+        word.insert(0, 'Password')
+
+def on_enter2(e):
+    first_name.delete(0, 'end')
+
+def on_leave2(e):
+    name = first_name.get()
+    if name == '':
+        first_name.insert(0, 'First Name')
+
+def on_enter3(e):
+    last_name.delete(0, 'end')
+
+def on_leave3(e):
+    name = last_name.get()
+    if name == '':
+        last_name.insert(0, 'Last Name')
+
+def on_enter4(e):
+    date_of_birth.delete(0, 'end')
+
+def on_leave4(e):
+    name = date_of_birth.get()
+    if name == '':
+        date_of_birth.insert(0, 'Date of Birth')
+
+frame = Frame(root, width=350, height=350, bg='white')
+frame.place(x=25, y=25)
+
+heading = Label(frame, text='Registration Form', fg='#0047ab', bg='white', font='Arimo,15,bold')
+heading.place(x=80, y=5)
+
+user = Entry(frame, width=25, fg='black', borderwidth=0, bg='white', font='Arimo,11')
+user.place(x=50, y=50)
+user.insert(0, 'Username')
+user.bind('<FocusIn>', on_enter)
+user.bind('<FocusOut>', on_leave)
+
+Frame(frame, width=295, height=2, bg='black').place(x=25, y=77)
+
+word = Entry(frame, width=25, fg='black', borderwidth=0, bg='white', font='Arimo,11', show='*')
+word.place(x=50, y=100)
+word.insert(0, 'Password')
+word.bind('<FocusIn>', on_enter1)
+word.bind('<FocusOut>', on_leave1)
+
+Frame(frame, width=295, height=2, bg='black').place(x=25, y=127)
+
+first_name = Entry(frame, width=25, fg='black', borderwidth=0, bg='white', font='Arimo,11')
+first_name.place(x=50, y=150)
+first_name.insert(0, 'First Name')
+first_name.bind('<FocusIn>', on_enter2)
+first_name.bind('<FocusOut>', on_leave2)
+
+Frame(frame, width=295, height=2, bg='black').place(x=25, y=177)
+
+last_name = Entry(frame, width=25, fg='black', borderwidth=0, bg='white', font='Arimo,11')
+last_name.place(x=50, y=200)
+last_name.insert(0, 'Last Name')
+last_name.bind('<FocusIn>', on_enter3)
+last_name.bind('<FocusOut>', on_leave3)
+
+Frame(frame, width=295, height=2, bg='black').place(x=25, y=227)
+
+date_of_birth = Entry(frame, width=25, fg='black', borderwidth=0, bg='white', font='Arimo,11')
+date_of_birth.place(x=50, y=250)
+date_of_birth.insert(0, 'Date of Birth')
+date_of_birth.bind('<FocusIn>', on_enter4)
+date_of_birth.bind('<FocusOut>', on_leave4)
+
+Frame(frame, width=295, height=2, bg='black').place(x=25, y=277)
+
+register_button = Button(frame, text='Register', width=15, bg='#0047ab', fg='white', font='Arimo, 11', command=register)
+register_button.place(x=120, y=310)
+
+root.mainloop()
